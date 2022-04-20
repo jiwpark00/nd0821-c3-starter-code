7d541c5ca1c9e33e8786d6bb66250002c72a253a
Ji Woong Park <jiwoongpark@Jis-iMac.local>
2022-04-20 13:44:31 -0400

    Updated example.png and main

--- starter/main.py
+++ starter/main.py
@@ -56,9 +56,6 @@ score_data = pd.read_csv('starter/data/first_100_test_inputs.csv')
 train_data = pd.read_csv('starter/data/train.csv')
 
 xgb = joblib.load("starter/model/final_xgb.pkl")
-encoder = joblib.load("starter/model/encoder.pkl")
-lb = joblib.load("starter/model/lb.pkl")
-
 
 @app.get("/")
 def welcome():
@@ -76,7 +73,8 @@ def return_prediction():
 	return {'Hi': score_data.shape}
 
 @app.post("/predict_dynamic")
-def predict(body: Value, encoder=encoder, lb=lb):
+def predict(body: Value):
+	print('hi')
 	body_dict = body.dict()
 
 	# Due to index issue
@@ -87,6 +85,9 @@ def predict(body: Value, encoder=encoder, lb=lb):
 # 	X_train, y_train, encoder, lb = process_data(
 #     train_data, categorical_features=cat_features, label="salary", training=True
 # )
+	
+	encoder = joblib.load("starter/model/encoder.pkl")
+	lb = joblib.load("starter/model/lb.pkl")
 
 	fixed_body_processed, y_test, encoder, lb = process_data(
     fixed_body_df, categorical_features=cat_features, label=None, training=False, encoder=encoder, lb=lb
